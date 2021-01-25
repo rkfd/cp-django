5e713a9888e26cfb2c1ef4e8659e5f42253b623a
Satwik Gawand <satwikgawand@gmail.com>
2021-01-25 22:33:14 +0530

    update polls: templating, and efficient context-aware routing

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,15 +1,19 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponse, Http404
 from .models import Question
 
 # Create your views here.
 def index(request):
   latest_question_list = Question.objects.order_by('-pub_date')[:5]
-  output = ', '.join([q.question_text for q in latest_question_list])
-  return HttpResponse(output)
+  
+  context = {
+    'latest_question_list': latest_question_list,
+  }
+  return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
-  return HttpResponse(f"You're looking at question {question_id}.")
+  question = get_object_or_404(Question, pk=question_id)
+  return render(request, 'polls/detail.html', {'question':question})
 
 def results(request, question_id):
   return HttpResponse(f"You're looking at the results of question {question_id}.")
